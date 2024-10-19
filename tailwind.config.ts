28a92ec260841f5d3acff71ee85cddf018310e0f
mstsurnalyakter <surnalyms@gmail.com>
2024-10-19 18:08:17 +0600

    material tailwind css setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,32 @@
-import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+export default withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        poppins: ' "Poppins", sans-serif',
+      },
+      keyframes: {
+        animatedgradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        gradient: "animatedgradient 6s ease infinite alternate",
       },
     },
   },
   plugins: [],
-};
-export default config;
+});
