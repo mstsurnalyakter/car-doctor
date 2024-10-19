268c76089dbcfe6431a1b29768fdba0cc244d7d9
mstsurnalyakter <surnalyms@gmail.com>
2024-10-19 18:38:41 +0600

    daisyui setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,19 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
+import type { Config } from "tailwindcss";
 
-/** @type {import('tailwindcss').Config} */
-export default withMT({
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {
-      fontFamily: {
-        poppins: ' "Poppins", sans-serif',
-      },
-      keyframes: {
-        animatedgradient: {
-          "0%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
-          "100%": { backgroundPosition: "0% 50%" },
-        },
-      },
-      backgroundSize: {
-        "300%": "300%",
-      },
-      animation: {
-        gradient: "animatedgradient 6s ease infinite alternate",
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
-});
+};
+export default config;
